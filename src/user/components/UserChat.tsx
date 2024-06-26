fcc26bdbd44d89ba6077b5359ad27ac04d968928
Paulo Pereira <wemakeagame@gmail.com>
2024-06-26 16:28:15 +0200

    video 52 chat style

--- src/user/components/UserChat.tsx
+++ src/user/components/UserChat.tsx
@@ -1,4 +1,4 @@
-import { Card } from "@radix-ui/themes";
+import { Card, Text, TextField } from "@radix-ui/themes";
 import * as Form from '@radix-ui/react-form';
 
 export type ChatMessage = { username: string, message: string };
@@ -8,29 +8,39 @@ type UserChatProps = {
     chat: ChatMessage[],
 }
 
+const getAlignmentMessage = (chatMessage: ChatMessage, username: string) => {
+    return chatMessage.username === username ? 'right' : 'left';
+}
+
 export const UserChat: React.FC<UserChatProps> = ({ username, chat }) => {
     return <Card>
-        <Card>
-            <>{chat.map(chatMessage => <p><strong>{`${chatMessage.username}: `}</strong>{chatMessage.message}</p>
-            )}</>
+        <Text>Chat</Text>
+        <Card style={{height :'200px', marginTop: '10px'}}>
+            <div style={{height :'150px', overflow:'auto', padding: '10px'}}>{chat.map(chatMessage => <p style={{ fontSize: '12px', fontWeight: 'normal', margin: '0', textAlign: getAlignmentMessage(chatMessage, username) }}>
+                <span style={{ fontSize: '15px', }}>
+                    <strong>{`${chatMessage.username}: `}</strong>
+                </span>
+                {chatMessage.message}</p>
+            )}</div>
         </Card>
         <Form.Root className="FormRoot">
             <Form.Field className="FormField" name="question">
-                <div style={{ display: 'flex', alignItems: 'baseline', justifyContent: 'space-between' }}>
-                    <Form.Label className="FormLabel">{username}</Form.Label>
-                    <Form.Message className="FormMessage" match="valueMissing">
-                        type your message...
-                    </Form.Message>
+                <div style={{ display: 'flex', alignItems: 'baseline', justifyContent: 'space-between', gap: '15px' }}>
+                    <Form.Control asChild style={{ flexGrow: 1 }}>
+                        <TextField.Root
+                            placeholder="type your message here"
+                            style={{ minWidth: '300px' }}
+                        />
+                    </Form.Control>
+                    <Form.Submit asChild>
+                        <button className="Button" style={{ marginTop: 10 }}>
+                            Send
+                        </button>
+                    </Form.Submit>
                 </div>
-                <Form.Control asChild>
-                    <textarea className="Textarea" required />
-                </Form.Control>
+
             </Form.Field>
-            <Form.Submit asChild>
-                <button className="Button" style={{ marginTop: 10 }}>
-                    Send
-                </button>
-            </Form.Submit>
+
         </Form.Root>
-    </Card>
+    </Card >
 }
